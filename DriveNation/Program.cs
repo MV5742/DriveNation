55fe6cf91a45a8eba92da4ad41bb2ea209dcb7e6
MV5742 <momchil.vidolov@abv.bg>
2024-04-05 18:45:49 +0300

    Fixed admin account not being automatically created

--- DriveNation/Program.cs
+++ DriveNation/Program.cs
@@ -19,7 +19,7 @@ namespace DriveNation
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<RentACarUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<RentACarUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -71,30 +71,30 @@ namespace DriveNation
             {
                 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<RentACarUser>>();
 
-                string email = "admin.adminov@gmail.com";
-                string username = "AdminAdminov3256";
-                string firstname = "Admin";
-                string lastname = "Adminov";
-                string password = "1qaz@WSX3edc$RFV";
-                string personal_id = "0525746574";
-                string phone = "+359918273645";
+                string email = "admin.adminov2@gmail.com";
+                string progileName = "AdminAdminov32562";
+                string firstname = "Admin2";
+                string lastname = "Adminov2";
+                string password = "1qaz@WSX3edc$RFV2";
+                string personal_id = "0525746542";
+                string phone = "+359918273642";
 
                 if (await userManager.FindByEmailAsync(email) == null)
                 {
                     var user = new RentACarUser();
+                    user.UserName = email;
                     user.Email = email;
-                    user.ProfileName = username;
+                    user.ProfileName = progileName;
                     user.FirstName = firstname;
                     user.LastName = lastname;
                     user.Personal_Id = personal_id;
                     user.PhoneNumber = phone;
-                    user.EmailConfirmed = true;
+                    user.EmailConfirmed = false;
 
                     await userManager.CreateAsync(user, password);
 
                     await userManager.AddToRoleAsync(user, "Admin");
                 }
-
             }
 
             app.Run();
